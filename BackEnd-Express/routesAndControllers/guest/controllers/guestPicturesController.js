79757f61a28ef7c671b7da09ef36b5442037f340
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-16 11:47:03 +0100

    Pictures (guest) show:OK

--- BackEnd-Express/routesAndControllers/guest/controllers/guestPicturesController.js
+++ BackEnd-Express/routesAndControllers/guest/controllers/guestPicturesController.js
@@ -2,6 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const ErrorFromDB = require("../../../exceptionsAndMiddlewares/exceptions/ErrorFromDB");
+const ErrorItemNotFound = require("../../../exceptionsAndMiddlewares/exceptions/ErrorItemNotFound");
 
 async function index(req, res, next)
 {
@@ -30,7 +31,33 @@ async function index(req, res, next)
 
 async function show(req, res, next)
 {
-
+    const id = parseInt(req.params.id);
+    console.log("ID RICHIESTO: ",id);
+    const prismaQuery = {   "where"     :   { 
+                                                "id"            :   id,
+                                                "visible"       :   true 
+                                            },
+                            "include"   :   {
+                                                "user"          :   true,
+                                                "categories"    :   true
+                                            }
+                        };
+    let pictureToFind = null;
+    try
+    {
+        pictureToFind = await prisma.Picture.findFirst(prismaQuery);
+        if (pictureToFind)
+        {
+            console.log("Picture cercata e trovata: ", pictureToFind);
+            res.json({ picture : pictureToFind });
+        }
+        else
+            return next( new ErrorItemNotFound("Picture non trovata") );
+    }
+    catch(error)
+    {
+        return next( new ErrorFromDB("Operazione non eseguibile al momento.") );
+    }
 }
 
 module.exports = { index, show } 
