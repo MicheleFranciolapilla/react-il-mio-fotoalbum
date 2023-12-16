127a5bcb39c304bf19f828aa5fa467c78c8205d2
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-16 17:59:40 +0100

    retrieveValidFilters funzionante

--- BackEnd-Express/utilities/filterUtilities/filteringFunctions.js
+++ BackEnd-Express/utilities/filterUtilities/filteringFunctions.js
@@ -4,24 +4,25 @@ const   adminFilters = require("./allowedFilters/adminFilters.json");
 function retrieveValidFilters(filtersFromBody, admin)
 {
     const allowedFilters = admin ? adminFilters : guestFilters;
-    const filtersKeys = Object.keys(filtersFromBody).map( bodyKey => bodyKey.trim().toLowerCase());
+    const filtersKeys = Object.keys(filtersFromBody).map( bodyKey => bodyKey);
     let finalFilters = [];
     console.log("*****************************************");
     console.log("***** RETRIEVE FUNCTION *****************");
     filtersKeys.forEach( bodyKey =>
         {
             console.log("KEY ATTUALE: ", bodyKey, " : ",filtersFromBody[bodyKey]);
-            const index = allowedFilters.findIndex( validFilter => Object.keys(validFilter).includes(bodyKey));
+            const bodyKeyLC = bodyKey.trim().toLowerCase();
+            const index = allowedFilters.findIndex( validFilter => Object.keys(validFilter).includes(bodyKeyLC));
             if (index >= 0)
             {
-                console.log("VALID FILTER: ", validFilter);
                 const validFilter = allowedFilters[index];
+                console.log("VALID FILTER: ", validFilter);
                 console.log
                 const validType = Object.values(validFilter)[0];
                 if  ((validType === "number") && (!isNaN(parseInt(filtersFromBody[bodyKey]))))
-                    finalFilters.push({ [bodyKey] : parseInt(filtersFromBody[bodyKey]) })
+                    finalFilters.push({ [bodyKeyLC] : parseInt(filtersFromBody[bodyKey]) })
                 else if (typeof filtersFromBody[bodyKey] === validType)
-                    finalFilters.push({ [bodyKey] : filtersFromBody[bodyKey] })
+                    finalFilters.push({ [bodyKeyLC] : filtersFromBody[bodyKey] })
             }
         });
     console.log("*****************************************");
