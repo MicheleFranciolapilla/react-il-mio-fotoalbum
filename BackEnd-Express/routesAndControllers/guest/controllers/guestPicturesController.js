4b40cf5522c63e3f0eff250a733fa646a70dac7f
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-16 11:02:03 +0100

    Pictures (guest) : index OK

--- BackEnd-Express/routesAndControllers/guest/controllers/guestPicturesController.js
+++ BackEnd-Express/routesAndControllers/guest/controllers/guestPicturesController.js
@@ -1,6 +1,31 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+const ErrorFromDB = require("../../../exceptionsAndMiddlewares/exceptions/ErrorFromDB");
+
 async function index(req, res, next)
 {
-
+    let itemsPerPage = 4;
+    const currentPage = req.query.page || 1;
+    let prismaQuery =   {   "where"     :   {   "visible"       :   true },
+                            "skip"      :   (currentPage - 1) * itemsPerPage, 
+                            "take"      :   itemsPerPage,
+                            "include"   :   {
+                                                "user"          :   true,
+                                                "categories"    :   true 
+                                            } 
+                        };
+    let pictures = [];
+    try
+    {
+        pictures = await prisma.Picture.findMany(prismaQuery);
+        console.log("PICTURES TROVATE ", pictures);
+        res.json({ "pictures" : pictures });
+    }
+    catch(error)
+    {
+        return next( new ErrorFromDB("Operazione non eseguibile al momento.") );
+    }
 }
 
 async function show(req, res, next)
