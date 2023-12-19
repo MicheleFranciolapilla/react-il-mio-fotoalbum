6f7cba1e354ca4391b5b64188b87d6fdefdc6a11
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-12-19 15:22:41 +0100

    Redirect da collection in caso di assenza pictures

--- FrontEnd-React/src/pages/PageCollection.jsx
+++ FrontEnd-React/src/pages/PageCollection.jsx
@@ -4,8 +4,10 @@ import { useContextApi } from "../contexts/ContextApi";
 import { useContextOverlay } from "../contexts/ContextOverlay";
 import { useContextDialog } from "../contexts/ContextDialog";
 import { useContextUserAuthentication } from "../contexts/ContextUserAuthentication";
+import { useNavigate } from "react-router-dom";
 
 import pagesStyle from "../assets/style/modules/styleForPages.module.css";
+import style from "../assets/style/modules/styleForCollectionPage.module.css";
 
 import { returnErrorMsg } from "../assets/utilities/errorRelatedFunctions";
 
@@ -13,12 +15,13 @@ export default function PageCollection()
 {
     const [pagingData, setPagingData] = useState({current_page : 1});
     const [validFilters, setValidFilters] = useState(null);
-    const [collection, setCollection] = useState([]);
+    const [collection, setCollection] = useState(null);
 
     const { getPictures } = useContextApi();
     const { incomingError, resetOverlay } = useContextOverlay();
     const { getDefaultDialogParams, dialogOn, dialogForError } = useContextDialog();
     const { userIsLogged } = useContextUserAuthentication();
+    const navigate = useNavigate();
 
     useEffect( () =>
         {
@@ -27,15 +30,19 @@ export default function PageCollection()
 
     async function makeApiCall()
     {
-        const response = await getPictures(pagingData.current_page, false);
+        // EFFETTUARE CONTROLLI SULLA PERSISTENZA DEGLI STATES AL CAMBIO DEL VALORE DI USERISLOGGED
+        const response = await getPictures(pagingData.current_page, userIsLogged);
         if (response.outcome)
         {
-            if (collection.length !== 0)
+            setCollection(response.data.pictures);
+            if (response.data.pictures.length !== 0)
                 setPagingData(response.data.paging_data);
             else if ((!pagingData) || (pagingData.current_page !== 1))
-                setPagingData({current_page : 1});
-            setValidFilters(response.data.valid_filters);
-            setCollection(response.data.pictures);
+                setPagingData({ current_page : 1 });
+            if (response.data.valid_filters) 
+                setValidFilters(response.data.valid_filters);
+            else
+                setValidFilters(null);
         }
         else
         {
@@ -49,12 +56,23 @@ export default function PageCollection()
                             "message1"      :   errorMsgs[0],
                             "message2"      :   errorMsgs[1],
                             "twoButtons"    :   false,
+                            "functionBtnOK" :   () => userIsLogged ? navigate("/dashboard") : navigate("/")
                         });
         }
     }
 
     return (
-        <div className={ pagesStyle.page }>
+        <div className={`${pagesStyle.page} flex justify-center items-center`}>
+            {
+                (collection) && <>
+                                    {
+                                        (collection.length === 0)   ?   <h2 className={ style.noDataMessage }>Nessuna foto disponibile</h2>
+                                                                    :   <>
+                                                                            
+                                                                        </>
+                                    }
+                                </>
+            }
         </div>
     )
 }
